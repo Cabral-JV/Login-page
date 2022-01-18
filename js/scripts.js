1bf897e795792d889bda722f29ac8ed7b7ef2a6c
Cabral-JV <jvccabral96@gmail.com>
2022-01-18 12:45:35 -0300

    add validation script

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,40 @@
-// $(document).ready(function () {
-    // var myCarousel = document.querySelector('#myCarousel')
-    // var carousel = new bootstrap.Carousel(myCarousel)
-// });
+$(document).ready(function () {
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel)
+});
+
+$("#register-form").validate({
+    rules: {
+        name: {
+            required: true,
+            minlength: 3
+        },
+        email: {
+            required: true
+        },
+        pass: {
+            required: true,
+            minlength: 6
+        },
+        mensagem: {
+            required: true
+        }
+    },
+    messages: {
+        name: {
+            required: "Por favor, informe seu nome",
+            minlength: "O nome deve ter pelo menos 3 caracteres"
+        },
+        email: {
+            required: "É necessário informar um email",
+            email: "Por favor insira um endereço de e-mail válido."
+        },
+        pass: {
+            required: "Por favor, informe sua senha",
+            minlength: "A senha deve ter pelo menos 6 caracteres"
+        },
+        mensagem: {
+            required: "A mensagem não pode ficar em branco"
+        }
+    }
+});
