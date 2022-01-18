7b3e7bd6736a5d303834aca1021a808624d62c89
Cabral-JV <jvccabral96@gmail.com>
2022-01-18 11:33:39 -0300

    add carousel and badges

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-/*!
-* Start Bootstrap - Shop Item v5.0.4 (https://startbootstrap.com/template/shop-item)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// $(document).ready(function () {
+    // var myCarousel = document.querySelector('#myCarousel')
+    // var carousel = new bootstrap.Carousel(myCarousel)
+// });
